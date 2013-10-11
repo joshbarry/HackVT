bbaaa5b51acd464748b90339a1959c43c757c1f2
Tyler J. Sawyer <aayrlgrump@gmail.com>
2013-10-11 16:54:58 -0400

    Map Update
    Updated map.php
    
    - added subdirectory 'map_scripts'
    -- includes map_helper.php and map_script.php

--- map.php
+++ map.php
@@ -1,5 +1,21 @@
 <?php 
+// --------------------------------------------------
+// Map Module 
+// Implements the GoogleMap API which displays our
+// school regions and allows users to interact with 
+// each region and explore dynamic information.
+//
+// Author(s): Tyler J. Sawyer, 
+//
+// --------------------------------------------------
 
-// map
+// DIV to help size map. Fix later. ~Tyler 
+echo '<div style="height:512px; width:512px;">';
 
+// Map script goes here
+include("map_scripts/map_script.php");
+// Map Helper goes here
+include("map_scripts/map_helper.php");
+
+echo '</div>';
 ?>
