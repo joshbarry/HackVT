bafc9c962793054cb60824c6a815beae086b9033
Tyler J. Sawyer <aayrlgrump@gmail.com>
2013-10-11 17:09:09 -0400

    Initial Map Script (Load Google Map)
    Show our google map baby.

--- map_scripts/map_script.php
+++ map_scripts/map_script.php
@@ -1,3 +1,34 @@
 <?php
-
+// ---------------------------------------------------------
+// GoogleMap API Implementation
+// Inserts our interactive GoogleMap into our webpage. We will use 
+// helper scripts to set up regions and interactive elements. This 
+// simply implements the map itself.
+//
+// Author: Tyler J. Sawyer
+// ---------------------------------------------------------
+//
+// SCRIPT SETTINGS
+// 
+// 
 ?>
+<!-- HTML Elements for GoogleMap Implementation -->
+<script type="text/javascript"
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBeqiMXfEQ_cpawUHH1Dvhmq0JXMa39vq4&amp;sensor=false">
+</script>
+<script type="text/javascript">
+// Geocoder Information
+var geocoder
+function initialize() {
+	// Center map on Burlington, Vermont
+	var mapCenter = new google.maps.LatLng(44.473840, -73.215637);
+	var myOptions = {
+		center: mapCenter,
+		zoom: 7,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	// Build Map
+	var map = new google.maps.Map(document.getElementById("map_content"), myOptions);
+	<?php //any php helper elements go here ?>
+}
+</script>
