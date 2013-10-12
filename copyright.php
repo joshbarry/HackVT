3538583e8ec781c57aef2ecffabd43953fbf7f49
Tyler J. Sawyer <aayrlgrump@gmail.com>
2013-10-12 01:13:33 -0400

    Fixed broken h3 tag. Added Copy symbol
    WHO FORGOT TO CLOSE h3? HE WAS SO COLD AND LONELY BECAUSE YOU LET ALL
    THE COOL AIR IN.

--- copyright.php
+++ copyright.php
@@ -1,3 +1,3 @@
 <?php
-echo '<h3>Copyright '.date("Y").' Team Karma - HackVT 2013. All Rights Reserved.<br />Version: 1.0'; 
+echo '<h3>Copyright &copy; '.date("Y").' Team Karma - HackVT 2013. All Rights Reserved.<br />Version: 1.0</h3>'; 
 ?>
