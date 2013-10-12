9df5c82b316471e6bc43cfcd7ff08222eaf01396
joshbarry92 <joshbarry92@gmail.com>
2013-10-11 23:08:16 -0400

    Test PHP Script
    TEST php script

---
+++ test.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+   <title>HackVT 2013 TITLE HERE</title>
+
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <meta name="author" content="Team Karma - HackVT 2013" />
+   <meta name="description" content="INSERT DATA HERE" />
+
+   <link rel="stylesheet" href="styleMain.css" type="text/css" media="screen" />
+   
+<?php
+
+$myServer = "HackVT.db.11612382.hostedresource.com"
+$myUser = "havkvt"
+$myPass = "hacked!7"
+$myDB = "HackVT"
+
+//connection to the database
+$dbhandle = mssql_connect($myServer, $myUser, $myPass)
+  or die("Couldn't connect to SQL Server on $myServer"); 
+
+//select a database to work with
+$selected = mssql_select_db($myDB, $dbhandle)
+  or die("Couldn't open database $myDB"); 
+
+//declare the SQL statement that will query the database
+$query = "SELECT DISTINCT OrgName ";
+$query .= "FROM dbo.Necap_Data ";
+$query .= "WHERE name='Academy School'"; 
+
+//execute the SQL query and return records
+$result = mssql_query($query);
+
+$numRows = mssql_num_rows($result); 
+echo $numRows; 
+
+//display the results 
+while($row = mssql_fetch_array($result))
+{
+  echo $row["id"];
+}
+//close the connection
+mssql_close($dbhandle);
+
+?>
+
+</head>
+
+<body>
+
+
+</body>
+</html>
