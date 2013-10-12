f99415a46e3913c4c56b38e5da65296a3410ec90
Ari Larson <ajlarson@uvm.edu>
2013-10-12 08:25:05 -0400

    made a start on some drop down menus

--- nav.php
+++ nav.php
@@ -1,6 +1,39 @@
-<?php
 
 //Navigation
 
-?>
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<body>
+<form name="navigation" action="" method=""> 
+<select name="Counties">
+	<option value="Addison">Addison</option>
+	<option value="Bennington">Bennington</option>
+	<option value="Caledonia">Caledonia</option>
+	<option value="Essex">Essex</option>
+	<option value="Franklin">Franklin</option>
+	<option value="Grand Isle">GrandIsle</option>
+	<option value="Lamoille">Lamoille</option>
+	<option value="Orange">Orange</option>
+	<option value="Orleans">Orleans</option>
+	<option value="Rutland">Rutland</option>
+	<option value="Washington">Washington</option>
+	<option value="Windham">Whindam</option>
+	<option value="Windsor">Windsor</option>
+</select>
 
+<select name="Tests">
+	<option value="Necap Science Grade 8 ">NECAP Science Grade 8</option>
+	<option value="NECAP Writing Grade 8">NECAP Writing Grade 8</option>
+	<option value="NECAP Science Grade 4">NECAP Science Grade 4</option>
+	<option value="NECAP Math Grades 3-8">NECAP Math Grades 38</option>
+	<option value="NECAP Reading Grades 38">NECAP Reading Grades 38</option>
+	<option value="NECAP Writing Grade 5">NECAP Writing Grade 5</option>
+	<option value="NECAP Writing Grade 11">NECAP Writing Grade 11</option>
+	<option value="NECAP Reading Grade 11">NECAP Reading Grade 11</option>
+	<option value="NECAP Science Grade 11">NECAP Science Grade 11</option>
+	<option value="NECAP Math Grade 11">NECAP Math Grade 11</option>
+</select>
+
+</form>
+</body>
+</html>
